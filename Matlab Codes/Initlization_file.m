1d67d021aa33f28ddde15f82143c8281982ee060
midhunks <mr.midhunks@gmail.com>
2017-03-28 19:25:51 -0400

    Immigration and initization files combined

--- Matlab Codes/Initlization_file.m
+++ Matlab Codes/Initlization_file.m
@@ -1,37 +1,35 @@
-function Cell = Initlization_file(Initial)
-global Mean_Cell_cycle Cell_cycle_Variability
+function Cell = Initlization_file(Type, Num_Cells)
+% Type = (1) for Donor cells and (2) for Recipient Cells'        
+% Num_Cells = How many cells needs to be created
 
+global Initial Mean_Cell_cycle Cell_cycle_Variability
+%% Null check
+if Num_Cells == 0
+    Cell = [];
+    return
+end
 %% Initial Age of each cell and Cell Cycle of each cell
 Age_flag = 1; %0 if all cells starts at zero age or 1 for a random age
-% Initial_Cell_Cycle = Mean_Cell_cycle + random('uniform',-1,1,Initial.Total_Cell_Population,1)*Cell_cycle_Variability;
-Initial_Cell_Cycle = random('normal',Mean_Cell_cycle,Cell_cycle_Variability,Initial.Total_Cell_Population,1);
-Initial_Cell_Clock = Age_flag*rand(Initial.Total_Cell_Population,1).*Initial_Cell_Cycle;
 
-%% Initial Plasmid population in Donor and Recipient cells
-%setting up the initial plasmid population in each type of cells
-E_Plasmid_population = poissrnd(Initial.Mean_E_Plasmid_Population,Initial.Total_Cell_Population,1);
-T_Plasmid_population = poissrnd(Initial.Mean_E_Plasmid_Population,Initial.Total_Cell_Population,1);
-
-%% Creating Initial Cell Structure
+%% Initilizing New Cells
 %Assigning type, number of plasmids, type of plasmids, age, cycle, and
 %generation to each cells
-
 Cell = struct;
-for i = 1:Initial.Total_Cell_Population
-    if i <= Initial.D_Cell_Population
-        Cell(i).Type = 1; %'Donor Cells';
-        Cell(i).E_Plasmid_Population = E_Plasmid_population(i);
+for i = 1:Num_Cells
+    if Type == 1 % Donors       
+        Cell(i).E_Plasmid_Population = poissrnd(Initial.Mean_E_Plasmid_Population,1);
         Cell(i).T_Plasmid_Population = 0;
-        % Cell(i).T_Plasmid_Population = T_Plasmid_population(i); % If Initially Donor Cells have some Target Plasmids
-    else
-        Cell(i).Type = 2;%'Recipient Cells';
+    elseif Type == 2 % Recipients      
         Cell(i).E_Plasmid_Population = 0;
-%         Cell(i).E_Plasmid_Population = E_Plasmid_population(i); % If Initially Target Cells have some Enginneered Plasmids
-        Cell(i).T_Plasmid_Population = T_Plasmid_population(i);
+%         Cell(i).E_Plasmid_Population = poissrnd(Initial.Mean_E_Plasmid_Population,1); % If Initially Target cells have engineered Plasmids
+        Cell(i).T_Plasmid_Population = poissrnd(Initial.Mean_E_Plasmid_Population,1);
+    else
+        error('Check the type of cells needs to be created ')
     end
     
-    Cell(i).Clock = Initial_Cell_Clock(i);
-    Cell(i).Cycle = Initial_Cell_Cycle(i);
+    Cell(i).Type = Type; 
+    Cell(i).Cycle = random('normal',Mean_Cell_cycle,Cell_cycle_Variability);
+    Cell(i).Clock = Age_flag*rand*Cell(i).Cycle;
     Cell(i).Generation = 1;
 end
 end
